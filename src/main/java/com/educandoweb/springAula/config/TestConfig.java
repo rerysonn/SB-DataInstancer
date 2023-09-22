0e5d550c87a192c1fd1a74ddd5062744ba87f560
rerysonn <rerysonfarinha@outlook.com>
2023-09-22 10:47:31 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/springAula/config/TestConfig.java
+++ src/main/java/com/educandoweb/springAula/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.springAula.entities.Order;
 import com.educandoweb.springAula.entities.User;
+import com.educandoweb.springAula.entities.enums.OrderStatus;
 import com.educandoweb.springAula.repositories.OrderRepository;
 import com.educandoweb.springAula.repositories.UserRepository;
 
@@ -36,9 +37,9 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Pedro goncalves", "pedro@gmail.com", "977389474", "345535");
 		
 		// INSTANCIAR O BANCO DE DADOS COM PEDIDOS ASSOCIADAS AOS USUARIOS //
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 		
 		// SALVAR AS INTANCIAS NO BANCO DE DADOS 
 		userRepository.saveAll(Arrays.asList(u1, u2));
--- src/main/java/com/educandoweb/springAula/entities/Order.java
+++ src/main/java/com/educandoweb/springAula/entities/Order.java
@@ -4,8 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.springAula.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -29,6 +29,8 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne // ASSOCIAÇÃO DE MUITOS PARA UM
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -36,10 +38,11 @@ public class Order implements Serializable{
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
+		setOrderStatus(orderStatus);
 	}
 
 	public Long getId() {
@@ -56,6 +59,16 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
