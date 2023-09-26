b6d03b86b83002453b8353d4df9350d9e45ff540
rerysonn <rerysonfarinha@outlook.com>
2023-09-25 21:49:22 -0300

    User Update

--- src/main/java/com/educandoweb/springAula/services/UserService.java
+++ src/main/java/com/educandoweb/springAula/services/UserService.java
@@ -34,5 +34,19 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	//ATUALIZAR O USUARIO
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
 
 }
